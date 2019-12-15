81e38d8ae923d7f7fde92c47b5bbe670a95f0888
Emily_Richard <richard.emily.e@gmail.com>
2019-12-14 23:11:13 -0500

    added state management

--- app/App.js
+++ app/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={ styles.screen }>
       <View style={ styles.inputContainer }>
-        <TextInput  placeholder='Course Goal'
-        style={ styles.input } />
-        <Button title="ADD" style={{ padding: 20 }} />
+        <TextInput 
+          placeholder='Course Goal'
+          style={ styles.input }
+          onChangeText = {goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button
+          title="ADD"
+          style={{ padding: 20 }}
+          onPress={addGoalHandler}
+        />
       </View>
       <View />
     </View>
